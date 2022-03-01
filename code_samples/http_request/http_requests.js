dfa0056966583181493a340580c0dda7c2a6cd46
Paulo-Brasko <97314450+Paulo-Brasko@users.noreply.github.com>
2022-02-28 22:48:40 -0500

    Update http_requests.js

--- code_samples/http_request/http_requests.js
+++ code_samples/http_request/http_requests.js
@@ -20,109 +20,68 @@ function addEventListeners() {
 }
 
 function retrieveAndDisplayAllBlogEntries() {
-    // issuing an HTTP Get request
-    const blogs = httpGetRequest(blogURL);
-    console.log(blogs);
+    // issuing an HTTP Get request to get all the blogs
+ 
 
     // getting a hold on the div to be used to populate the page with the blogs
-    let displayDivElement = document.getElementById("displayDivId");
-    displayDivElement.innerHTML = "";
+
 
     // looping over all the blogs and creating the necessary HTML elements
-    for (let blog of blogs) {
-        let titleElement = document.createElement("h2");
-        titleElement.innerText = `${blog.id} - Title: ${blog.title}`;
 
-        const authorElement = document.createElement("h3");
-        authorElement.innerText = `Author: ${blog.author}`;
-
-        const breakElement = document.createElement("br");
-
-        displayDivElement.append(titleElement);
-        displayDivElement.append(authorElement);
-        displayDivElement.append(breakElement);
-    }
 }
 
 function retrieveAndDisplayOneBlogEntry() {
     // getting a hold on the blogNumber input element
-    const blogNumberElement = document.getElementById("blogNumberTextInput");
-    const blogNumber = blogNumberElement.value;
 
+    
     // issuing an HTTP Get Request with "/blogNumber" added to the blogURL
-    const blog = httpGetRequest(`${blogURL}/${blogNumber}`);
 
+    
     // creating the necessary HTML elements to display the blog on the page
-    let titleElement = document.createElement("h2");
-    titleElement.innerText = `Title: ${blog.title}`;
 
-    const authorElement = document.createElement("h3");
-    authorElement.innerText = `Author: ${blog.author}`;
-
-    const blogContentElement = document.createElement("p");
-    blogContentElement.innerText = `${blog.body}`;
-
-    const breakElement = document.createElement("br");
-
-    // adding the blog elements into the single blog div placeholder on the page
-    let displayDivElement = document.getElementById("displayDivId");
-    displayDivElement.innerHTML = "";
-    displayDivElement.append(titleElement);
-    displayDivElement.append(authorElement);
-    displayDivElement.append(blogContentElement);
-    displayDivElement.append(breakElement);
 }
 
 function addOneBlogEntry() {
     // getting a hold on the blog elements title, author, and body from the page
-    const newBlogTitleElement = document.getElementById("insertNewBlogTitleTextInput");
-    const newBlogAuthorElement = document.getElementById("insertNewBlogAuthorTextInput");
-    const newBlogBodyElement = document.getElementById("insertNewBlogBodyTextInput");
 
+    
     // creating a blog object
-    let newBlog = {};
-    newBlog["title"] = `${newBlogTitleElement.value}`;
-    newBlog["author"] = `${newBlogAuthorElement.value}`;
-    newBlog["body"] = `${newBlogBodyElement.value}`;
 
+    
     // issuing an HTTP Post Request
-    httpPostRequest(blogURL, newBlog);
 
-    let displayDivElement = document.getElementById("displayDivId");
-    displayDivElement.innerHTML = "";
+    
+    // emptying the display area
+
+
 }
 
 function changeExistingBlog() {
     // getting a hold on the blog number to be updated
-    let changeExistingBlogNumberTextInput = document.getElementById("changeExistingBlogNumberTextInput");
-    let existingBlogNumber = changeExistingBlogNumberTextInput.value;
 
+    
     // getting a hold on the contents of the updated blog from the page
-    let changeExistingBlogTitleTextInput = document.getElementById("changeExistingBlogTitleTextInput");
-    let changeExistingBlogAuthorTextInput = document.getElementById("changeExistingBlogAuthorTextInput");
-    let changeExistingBlogBodyTextInput = document.getElementById("changeExistingBlogBodyTextInput");
 
+    
     // creating the blog that will replace the contents of a previously stored blog
-    let updatedBlog = {};
-    updatedBlog["title"] = `${changeExistingBlogTitleTextInput.value}`;
-    updatedBlog["author"] = `${changeExistingBlogAuthorTextInput.value}`;
-    updatedBlog["body"] = `${changeExistingBlogBodyTextInput.value}`;
 
+    
     // issuing an HTTP PUT request
-    httpPutRequest(`${blogURL}/${existingBlogNumber}`, updatedBlog);
+    
 
-    let displayDivElement = document.getElementById("displayDivId");
-    displayDivElement.innerHTML = "";
+    // emptying the display area
+
 }
 
 function deleteExistingBlog() {
-    let deleteExistingBlogNumberTextInput = document.getElementById("deleteExistingBlogNumberTextInput");
-    let existingBlogNumber = deleteExistingBlogNumberTextInput.value;
+    // getting the number of the blog to be deleted
 
-    httpDeleteRequest(`${blogURL}/${existingBlogNumber}`);
+    
+    // issuing a delete request for that given blog number
 
-    let displayDivElement = document.getElementById("displayDivId");
-    displayDivElement.innerHTML = "";
+    
+    // emptying the display area
+
 }
 
 function httpGetRequest(theUrl) {
